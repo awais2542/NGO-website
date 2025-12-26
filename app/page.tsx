9c33078fb4f23f98ef485da7c3aa2ec4615cfdcd
awais2542 <malikawais2542@gmail.com>
2025-12-26 21:49:26 +0500

    project-creation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-   <main><h1>Hello world</h1></main>
+   <main><h1 className="text-3xl font-bold underline p-10 text-chart  text-primary">Hello world</h1>
+   <Button variant={'default'} size={'icon-lg'}>Click</Button>
+   </main>
   )
 }
+ 
